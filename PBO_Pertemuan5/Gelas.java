f234fccbde61606f98b01fd9ee92707f47654e48
Adirach08 <121677961+Adirach08@users.noreply.github.com>
2024-03-13 16:57:38 +0700

    Pertemuan 5

---
+++ PBO_Pertemuan5/Gelas.java
@@ -0,0 +1,19 @@
+package PBO_Pertemuan5;
+
+public class Gelas {
+	
+	private String warna;
+
+    public Gelas(String w) {
+        warna = w;
+    }
+
+    void setWarna(String w) {
+        warna = w;
+    }
+
+    String getWarna() {
+        return warna;
+    }
+    
+}
