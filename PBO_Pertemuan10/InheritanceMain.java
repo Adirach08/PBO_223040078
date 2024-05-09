27875cf4cc65b04fcceb2e211ecca1692bc1d4b7
Adirach08 <121677961+Adirach08@users.noreply.github.com>
2024-05-09 14:59:56 +0700

    Delete PBO_Pertemuan10 directory

--- PBO_Pertemuan10/InheritanceMain.java
+++
@@ -1,8 +0,0 @@
-package PBO_Pertemuan10;
-
-public class InheritanceMain {
-    public static void main(String[] args) {
-        Mahasiswa mhs = new Mahasiswa("1001", "Ade Sukendar", "Bandung");
-        System.out.println(mhs.getNrp() + "-" + mhs.getNama() + "_" + mhs.getAlamat());
-    }
-}
