15e04f1e755c525b7f70b5e967ba9d665e4d4b53
Adirach08 <121677961+Adirach08@users.noreply.github.com>
2024-05-09 15:01:03 +0700

    Mengerjakan Tugas Inheritance

---
+++ PBO_Pertemuan10/Karyawan.java
@@ -0,0 +1,19 @@
+package PBO_Pertemuan10;
+
+public class Karyawan {
+    protected String nama;
+    protected String alamat;
+    
+    public Karyawan (String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getAlamat() {
+        return alamat;
+    }
+}
