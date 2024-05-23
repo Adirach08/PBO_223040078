a6acabd9425b9e343f00317415b7315555f19767
Adirach08 <121677961+Adirach08@users.noreply.github.com>
2024-05-23 21:31:04 +0700

    Mengerjakan Tugas Abstract dan Interface

---
+++ PBO_Pertemuan11/AbstractMainApp.java
@@ -0,0 +1,18 @@
+package PBO_Pertemuan11;
+
+public class AbstractMainApp {
+    public static void main(String[] args) {
+        // Create a Lingkaran object with jari-jari 10
+        Lingkaran lingkaran = new Lingkaran(10);
+        System.out.println("Lingkaran:");
+        System.out.println("Jari-jari: " + lingkaran.getJari2());
+        System.out.println("Luas: " + lingkaran.luas());
+
+        // Create a Tabung object with jari-jari 10 and tinggi 5
+        Tabung tabung = new Tabung(10, 5);
+        System.out.println("\nTabung:");
+        System.out.println("Jari-jari: " + tabung.getJari2());
+        System.out.println("Tinggi: " + tabung.getTinggi());
+        System.out.println("Luas permukaan: " + tabung.luas());
+    }
+}
